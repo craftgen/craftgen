5096ba243b966f082e6e3bc9bb65e78cc2a8de47
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2023-09-12 16:16:32 +0700

    fix plate

--- apps/web/src/components/plate-ui/fixed-toolbar-buttons.tsx
+++ apps/web/src/components/plate-ui/fixed-toolbar-buttons.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React from "react";
 import {
   MARK_BOLD,
   MARK_CODE,
   MARK_ITALIC,
   MARK_STRIKETHROUGH,
   MARK_UNDERLINE,
-} from '@udecode/plate-basic-marks';
-import { usePlateReadOnly } from '@udecode/plate-common';
+} from "@udecode/plate-basic-marks";
+import { usePlateReadOnly } from "@udecode/plate-common";
 
-import { Icons } from '@/components/icons';
+import { Icons } from "@/components/icons";
 
-import { InsertDropdownMenu } from './insert-dropdown-menu';
-import { MarkToolbarButton } from './mark-toolbar-button';
-import { ModeDropdownMenu } from './mode-dropdown-menu';
-import { ToolbarGroup } from './toolbar';
-import { TurnIntoDropdownMenu } from './turn-into-dropdown-menu';
+import { InsertDropdownMenu } from "./insert-dropdown-menu";
+import { MarkToolbarButton } from "./mark-toolbar-button";
+import { ModeDropdownMenu } from "./mode-dropdown-menu";
+import { ToolbarGroup } from "./toolbar";
+import { TurnIntoDropdownMenu } from "./turn-into-dropdown-menu";
+import { LinkToolbarButton } from "./link-toolbar-button";
+import { AlignDropdownMenu } from "./align-dropdown-menu";
+import { MediaToolbarButton } from "./media-toolbar-button";
+import { ELEMENT_IMAGE } from "@udecode/plate-media";
+import { TableDropdownMenu } from "./table-dropdown-menu";
+import { EmojiDropdownMenu } from "./emoji-dropdown-menu";
+import { CommentToolbarButton } from "./comment-toolbar-button";
+import { ArticleSaveButton } from "./article-save-button";
 
-export function FixedToolbarButtons() {
+export function FixedToolbarButtons({ id }: { id: string }) {
   const readOnly = usePlateReadOnly();
 
   return (
     <div className="w-full overflow-hidden">
       <div
-        className="flex flex-wrap"
+        className="flex flex-wrap @container"
         style={{
-          transform: 'translateX(calc(-1px))',
+          transform: "translateX(calc(-1px))",
         }}
       >
         {!readOnly && (
@@ -57,6 +65,17 @@ export function FixedToolbarButtons() {
               <MarkToolbarButton tooltip="Code (⌘+E)" nodeType={MARK_CODE}>
                 <Icons.code />
               </MarkToolbarButton>
+              <ToolbarGroup>
+                <AlignDropdownMenu />
+              </ToolbarGroup>
+              <ToolbarGroup>
+                <LinkToolbarButton />
+                <MediaToolbarButton nodeType={ELEMENT_IMAGE} />
+                <TableDropdownMenu />
+                <EmojiDropdownMenu />
+
+                {/* <PlaygroundMoreDropdownMenu /> */}
+              </ToolbarGroup>
             </ToolbarGroup>
           </>
         )}
@@ -64,6 +83,8 @@ export function FixedToolbarButtons() {
         <div className="grow" />
 
         <ToolbarGroup noSeparator>
+          <CommentToolbarButton />
+          <ArticleSaveButton id={id} />
           <ModeDropdownMenu />
         </ToolbarGroup>
       </div>
