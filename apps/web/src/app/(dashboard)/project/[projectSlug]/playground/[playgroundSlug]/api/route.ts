243ebbb05446e7e053892cf8476057fbcf089b7f
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2023-09-25 14:37:03 +0700

    api v0.002

--- apps/web/src/app/(dashboard)/project/[projectSlug]/playground/[playgroundSlug]/api/route.ts
+++ apps/web/src/app/(dashboard)/project/[projectSlug]/playground/[playgroundSlug]/api/route.ts
@@ -20,35 +20,16 @@ export async function POST(
   const a = JSON.parse(
     '{"value":"connected","done":false,"context":{"moduleId":"ef1de145-35bf-476c-b5ce-c038ecd86c9b","inputId":"3f0d7e8e-d0b9-4fba-9da8-8ff14c25de17","inputs":[{"name":"title","type":"string","description":""}],"outputs":["input"],"error":null,"inputData":{"title":["1f1f"]},"outputData":{"input":["Hello 1f1f"]}},"historyValue":{},"_internalQueue":[],"children":{},"tags":[],"meta":{}}'
   );
-  const module = new ModuleNode(di as any, { state: a } as any);
-  console.log("module", module.module);
-  while (!module.module) {
-    await new Promise((resolve) => setTimeout(resolve, 100));
+  const craft = new ModuleNode(di as any, { state: a } as any);
+  console.log("module", craft.module);
+  while (!craft.module) {
+    await new Promise((resolve) => setTimeout(resolve, 10));
   }
-  const data = await module.module?.exec(
+  const data = await craft.module?.exec(
     "3f0d7e8e-d0b9-4fba-9da8-8ff14c25de17",
     input
   );
   console.log("res", data);
-  // console.log({
-  //   module,
-  // });
-  // module.actor.send({
-  //   type: "RUN",
-  //   inputData: {
-  //     title: ["neco"],
-  //   },
-  // });
-  // const data = await new Promise((resolve) => {
-  //   module.actor.subscribe((state) => {
-  //     console.log("STATE", state.value);
-  //     console.log("STATE", state.context);
-  //     if (state.matches("complete")) {
-  //       console.log("COMPLETE", { message: state.context.message });
-  //       resolve(state.context);
-  //     }
-  //   });
-  // });
 
   return NextResponse.json({
     hello: "world",
