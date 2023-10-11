536cc80ccef3b3613b9dcca8ebf9d92fea86f5c0
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2023-10-11 14:48:01 +0700

    wii

---
+++ apps/web/src/jobs/workflow.ts
@@ -0,0 +1,57 @@
+import { eventTrigger } from "@trigger.dev/sdk";
+import { client } from "@/trigger";
+import { z } from "zod";
+import { getWorkflow } from "@/app/(dashboard)/project/[projectSlug]/playground/[playgroundSlug]/action";
+
+client.defineJob({
+  id: "execute-workflow",
+  name: "Execute Workflow",
+  version: "0.0.1",
+  trigger: eventTrigger({
+    name: "workflow.execute",
+    schema: z.object({
+      workflowSlug: z.string(),
+      projectSlug: z.string(),
+      version: z.string(),
+    }),
+  }),
+  run: async (payload, io, ctx) => {
+    const workflow = await getWorkflow({
+      workflowSlug: payload.workflowSlug,
+      projectSlug: payload.projectSlug,
+      version: Number(payload.version),
+    });
+
+    if (!workflow.data) {
+      throw new Error("Workflow not found");
+    }
+  },
+});
+
+client.defineJob({
+  id: "execute-workflow-step",
+  name: "Execute Workflow",
+  version: "0.0.1",
+  trigger: eventTrigger({
+    name: "workflow.execute",
+    schema: z.object({
+      workflowSlug: z.string(),
+      projectSlug: z.string(),
+      version: z.string(),
+      workflowNodeId: z.string(),
+      workflowExecutionId: z.string(),
+    }),
+  }),
+  run: async (payload, io, ctx) => {
+    const workflow = await getWorkflow({
+      workflowSlug: payload.workflowSlug,
+      projectSlug: payload.projectSlug,
+      version: Number(payload.version),
+      executionId: payload.workflowExecutionId,
+    });
+
+    if (!workflow.data) {
+      throw new Error("Workflow not found");
+    }
+  },
+});
