e9af8112ce7a66ced17c25127172e789db8f7651
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2023-11-11 13:30:44 +0800

    fix: :art:
    thread intial

--- apps/web/src/core/ui/control/control-thread.tsx
+++ apps/web/src/core/ui/control/control-thread.tsx
@@ -1,8 +1,14 @@
-import { api } from "@/trpc/react";
 import { ThreadControl } from "@seocraft/core/src/controls/thread";
 
+import { api } from "@/trpc/react";
+
 export const ThreadControlComponent = (props: { data: ThreadControl }) => {
-  api.openai.
+  const { data: thread } = api.openai.tread.retrieve.useQuery(
+    { threadId: props.data.threadId },
+    {
+      enabled: !!props.data.threadId,
+    },
+  );
   return (
     <div>
       <div>ThreadControlComponent</div>
