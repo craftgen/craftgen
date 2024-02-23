c859140356ecf36bb8458fc07da7ae3e0fcab569
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-02-23 11:42:23 +0800

    identify on the web as well

--- apps/web/src/app/(dashboard)/hooks/use-user.tsx
+++ apps/web/src/app/(dashboard)/hooks/use-user.tsx
@@ -2,10 +2,18 @@
 
 import { api } from "@/trpc/react";
 import { RouterOutputs } from "@/trpc/shared";
+import posthog from "posthog-js";
 
 export const useUser = (params?: {
   fallbackData?: RouterOutputs["auth"]["getSession"];
 }) => {
   const res = api.auth.getSession.useQuery();
+  if (res.data) {
+    posthog.identify(res.data.user.id, {
+      email: res.data.user.email,
+      name: res.data.user.user_metadata?.full_name,
+      username: res.data.username,
+    });
+  }
   return res;
 };
