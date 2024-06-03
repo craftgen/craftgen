ec9a1bb6ef106b2f31cbf1c6140a1db3ae2d3ca2
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-06-03 12:31:19 +0800

    tailwindy

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -118,6 +118,9 @@ module.exports = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+        "tremor-small": "0.375rem",
+        "tremor-default": "0.5rem",
+        "tremor-full": "9999px",
       },
       keyframes: {
         "accordion-down": {
@@ -191,11 +194,6 @@ module.exports = {
           "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
         "dark-tremor-dropdown":
           "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-      },
-      borderRadius: {
-        "tremor-small": "0.375rem",
-        "tremor-default": "0.5rem",
-        "tremor-full": "9999px",
       },
       fontSize: {
         "tremor-label": ["0.75rem"],
