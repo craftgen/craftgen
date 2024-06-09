fec58c5327e41bc2fb12c5f89a2c1ccdf8473902
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-06-09 13:40:42 +0800

    better structure

--- apps/web/src/app/(public)/docs/layout.tsx
+++ apps/web/src/app/(public)/docs/layout.tsx
@@ -1,11 +1,27 @@
+import { DiscordLogoIcon } from "@radix-ui/react-icons";
 import { pageTree } from "../../source";
 import { DocsLayout } from "fumadocs-ui/layout";
 import type { ReactNode } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout
       tree={pageTree}
+      sidebar={{
+        banner: (
+          <div className="flex flex-col w-full">
+            <Link href="/discord">
+              <Button variant={"outline"} className="w-full">
+                <DiscordLogoIcon className="mr-4" />
+                Community
+              </Button>
+            </Link>
+          </div>
+        ),
+        // defaultOpenLevel: 2,
+      }}
       nav={{
         enabled: false,
       }}
