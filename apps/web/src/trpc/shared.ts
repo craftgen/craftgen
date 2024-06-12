590936305e3e08e9c1c225b54aa2fdafa62f064b
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-06-12 14:57:19 +0800

    fix base url for trpc

--- apps/web/src/trpc/shared.ts
+++ apps/web/src/trpc/shared.ts
@@ -3,16 +3,19 @@ import superjson from "superjson";
 
 import type { AppRouter } from "@craftgen/api";
 
+import { BASE_URL } from "@/lib/constants";
+
 export const transformer = superjson;
 
-function getBaseUrl() {
-  if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+// function getBaseUrl() {
+//   if (typeof window !== "undefined") return "";
+//   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+//   return `http://localhost:${process.env.PORT ?? 3000}`;
+// }
 
 export function getUrl() {
-  return getBaseUrl() + "/api/trpc";
+  return `${BASE_URL}` + "/api/trpc";
 }
 
 /**
