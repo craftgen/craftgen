183e0a94652d87c0ad65eda226f54ce8154afebe
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-06-27 09:00:09 +0800

    add sentry for desktop

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -27,7 +27,21 @@ struct AppState {
 }
 
 fn main() {
+    let client = tauri_plugin_sentry::sentry::init((
+        "https://9d430bec9a3518bcb0c34c7f8b9fe1d8@o4507501119799296.ingest.us.sentry.io/4507501176029184",
+        tauri_plugin_sentry::sentry::ClientOptions {
+            release: tauri_plugin_sentry::sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+       // Everything before here runs in both app and crash reporter processes
+       let _guard = tauri_plugin_sentry::minidump::init(&client);
+       // Everything after here runs in only the app process
+
+
     tauri::Builder::default()
+
+        .plugin(tauri_plugin_sentry::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_oauth::init())
         .plugin(
