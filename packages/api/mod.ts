6821d29e3a38e4200b77d263073ed8ef51fd2acb
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-07-10 12:43:31 +0800

    root methdo

--- packages/api/mod.ts
+++ packages/api/mod.ts
@@ -1,10 +1,14 @@
+import { z } from "./deps.ts";
 import { helloRouter } from "./router/hello.ts";
-import { createTRPCRouter } from "./trpc.ts";
+import { createTRPCRouter, publicProcedure } from "./trpc.ts";
 
 export { createTRPCContext } from "./trpc.ts";
 
 export const appRouter = createTRPCRouter({
   hello: helloRouter,
+  hey: publicProcedure.input(z.string().nullish()).query(({ input }) => {
+    return `Hey ${input ?? "World"}!`;
+  }),
 });
 
 // export type definition of API
