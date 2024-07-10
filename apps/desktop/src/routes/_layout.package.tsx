256b53b7ec14bdf07ec478ef43d527c0a0bfcd08
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-07-10 11:06:17 +0800

    fix router

--- apps/desktop/src/routes/_layout.package.tsx
+++ apps/desktop/src/routes/_layout.package.tsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { Command } from "@tauri-apps/plugin-shell";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import superjson from "superjson";
 
+import { type AppRouter } from "@craftgen/ipc-api";
 import { Button } from "@craftgen/ui/components/button";
 import { JSONView } from "@craftgen/ui/components/json-view";
+
+const client = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      transformer: superjson,
+      url: "http://localhost:24321/trpc",
+    }),
+  ],
+});
 
 const PackagePage = () => {
   const [data, setData] = useState({});
@@ -21,9 +33,18 @@ const PackagePage = () => {
     setData(result);
   };
 
+  const rpcTest = async () => {
+    // const result = await client.hello.({
+    //   input: "World",
+    // });
+    // console.log("RESULT", result);
+    // setData(result);
+  };
+
   return (
     <div>
       <Button onClick={runtest}>Run Test</Button>
+      <Button onClick={rpcTest}>Run Test</Button>
       <JSONView src={data} />
     </div>
   );
--- packages/api/router/hello.ts
+++ packages/api/router/hello.ts
@@ -1,12 +1,8 @@
-import { initTRPC } from "npm:@trpc/server@10.45.2";
 import { z } from "npm:zod@3.23.8";
 
-const t = initTRPC.create();
+import { createTRPCRouter, publicProcedure } from "../trpc.ts";
 
-const publicProcedure = t.procedure;
-const router = t.router;
-
-export const helloRouter = router({
+export const helloRouter = createTRPCRouter({
   hello: publicProcedure.input(z.string().nullish()).query(({ input }) => {
     return `Hello ${input ?? "World"}!`;
   }),
