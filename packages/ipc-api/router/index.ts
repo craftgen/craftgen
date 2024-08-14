9a6790ee5d9a9e049b27311bf87f3046965b0e51
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-08-14 14:48:05 +0800

    relacote the api

--- packages/ipc-api/router/index.ts
+++ packages/ipc-api/router/index.ts
@@ -3,7 +3,7 @@ import { createTRPCRouter, publicProcedure } from "../trpc.ts";
 import { fsRouter } from "./file.ts";
 import { packageRouter } from "./package.ts";
 
-import "@craftgen/api";
+// import {create} from "@craftgen/api";
 
 export const appRouter = createTRPCRouter({
   package: packageRouter,
