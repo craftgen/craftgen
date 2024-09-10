a27ae62b087489ad1387122c0ea4f41c00ebec39
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2024-09-10 08:57:46 +0800

    move web to next

--- apps/next/src/lib/constants.ts
+++ apps/next/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
   ? process.env.NEXT_PUBLIC_BASE_URL
   : process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000";
